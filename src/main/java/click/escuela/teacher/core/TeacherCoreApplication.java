039ac50070cc57d116a95f85a24504a1f29c379a
umbert oscar <oscar.umbert@gmail.com>
2021-05-08 17:19:25 -0300

    se crea repo

---
+++ src/main/java/click/escuela/teacher/core/TeacherCoreApplication.java
@@ -0,0 +1,13 @@
+package click.escuela.teacher.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherCoreApplication.class, args);
+	}
+
+}
