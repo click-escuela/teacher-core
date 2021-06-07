64af4cc16abe655b505006b984d8d67ce5eb4d56
Tony-L-93 <tonyliendro@gmail.com>
2021-06-06 23:22:32 -0300

    Changes

--- src/main/java/click/escuela/teacher/core/api/ActivityApi.java
+++ src/main/java/click/escuela/teacher/core/api/ActivityApi.java
@@ -35,15 +35,6 @@ public class ActivityApi {
 	@JsonProperty(value = "name", required = true)
 	private String name;
 
-	@NotNull(message = "School Id cannot be null")
-	@JsonProperty(value = "schoolId", required = true)
-	private Integer schoolId;
-
-	@NotBlank(message = "Course Id cannot be empty")
-	@Size(max = 50, message = "Course Id must be less than 50 characters")
-	@JsonProperty(value = "courseId", required = true)
-	private String courseId;
-
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@JsonProperty(value = "dueDate", required = true)
 	private LocalDate dueDate;
@@ -61,5 +52,14 @@ public class ActivityApi {
 	@Size(max = 50, message = "Description must be less than 50 characters")
 	@JsonProperty(value = "description", required = true)
 	private String description;
+
+	@NotNull(message = "School Id cannot be null")
+	@JsonProperty(value = "schoolId", required = true)
+	private Integer schoolId;
+
+	@NotBlank(message = "Course Id cannot be empty")
+	@Size(max = 50, message = "Course Id must be less than 50 characters")
+	@JsonProperty(value = "courseId", required = true)
+	private String courseId;
 
 }
