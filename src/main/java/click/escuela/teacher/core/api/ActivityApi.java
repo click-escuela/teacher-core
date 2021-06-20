f82e4b0ca99ee21c9d62ba0c527dee33e98d1f58
Tony-L-93 <tonyliendro@gmail.com>
2021-06-20 00:24:56 -0300

    Code Smell fixed

--- src/main/java/click/escuela/teacher/core/api/ActivityApi.java
+++ src/main/java/click/escuela/teacher/core/api/ActivityApi.java
@@ -39,11 +39,6 @@ public class ActivityApi {
 	@JsonProperty(value = "schoolId", required = true)
 	private Integer schoolId;
 
-	@NotBlank(message = "Course Id cannot be empty")
-	@Size(max = 50, message = "Course Id must be less than 50 characters")
-	@JsonProperty(value = "courseId", required = true)
-	private String courseId;
-
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@JsonProperty(value = "dueDate", required = true)
 	private LocalDate dueDate;
@@ -61,5 +56,10 @@ public class ActivityApi {
 	@Size(max = 50, message = "Description must be less than 50 characters")
 	@JsonProperty(value = "description", required = true)
 	private String description;
+	
+	@NotBlank(message = "Course Id cannot be empty")
+	@Size(max = 50, message = "Course Id must be less than 50 characters")
+	@JsonProperty(value = "courseId", required = true)
+	private String courseId;
 
 }
