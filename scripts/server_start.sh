8d4f212f3ea8e32444f70d6530133fc07b668ff2
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-22 12:14:09 -0300

    fix de scripts

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/teacher-core
+
+sudo rm -rf /home/ec2-user/server/teacher-core/teacher-service.pid
+
+echo "eliminando archivo"
+
 sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
     teacher.core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > teacher-service.pid
