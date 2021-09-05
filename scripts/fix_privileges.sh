45a0d6c3a324f7b2988b2ac7d93301d43a2ef083
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 21:04:48 -0300

    fix logs

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -5,6 +5,4 @@ chmod +x /home/ec2-user/server/teacher-core/server_start.sh
 chmod +x /home/ec2-user/server/teacher-core/server_stop.sh
 chmod +x /home/ec2-user/server/teacher-core/server_clear.sh
 chmod +x /home/ec2-user/server/teacher-core/fix_privileges.sh
-chmod +x /home/ec2-user/server/teacher-core/logs
-chmod +x /home/ec2-user/server/teacher-core/logs/error.log
-chmod +x /home/ec2-user/server/teacher-core/logs/debug.log
+
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/teacher-core/logs
+chmod +x /home/ec2-user/server/teacher-core/logs/error.log
+chmod +x /home/ec2-user/server/teacher-core/logs/debug.log
 var="$(cat /home/ec2-user/server/teacher-core/teacher-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/teacher-core/teacher-service.pid
