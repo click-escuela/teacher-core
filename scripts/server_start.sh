eb9fffb9f20a3cac4917ad53146058208289c17d
Tu Nombre <eve_aragon@hotmail.com>
2021-10-13 13:52:25 -0300

    subir a produccion

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/teacher-core
 
-sudo rm -rf /home/ec2-user/server/teacher-core/teacher-service.pid
-
-echo "eliminando archivo"
-
-sudo java -jar -Dspring.profiles.active=prod -Dlogging.file.name=/home/ec2-user/server/teacher-core/debug.log \
-    teacher.core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > teacher-service.pid
+sudo sh server_start.sh
