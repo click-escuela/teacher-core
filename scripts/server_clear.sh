fb25bd10c3fb41f748fb279faf5a620ec33c15c2
Evelin <eve_aragon@hotmail.com>
2021-10-16 17:42:31 -0300

    mejora de scripts

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -5,5 +5,6 @@ sudo rm -rf /home/ec2-user/server/teacher-core/teacher-service.pid
 sudo rm -rf /home/ec2-user/server/teacher-core/logs
 sudo rm -rf /home/ec2-user/server/teacher-core/server_stop.sh
 sudo rm -rf /home/ec2-user/server/teacher-core/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/teacher-core/server_prestart.sh
 sudo rm -rf /home/ec2-user/server/teacher-core/server_clear.sh
 
